73e097e2b9734adad5919ddf4e95d09403852e1b
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-03-22 23:23:19 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let myArray = [];
+
+
+    if (!Array.isArray(arr)) throw new Error();
+
+
+    if (arr.length == 0) return arr;
+
+
+    for (let m = 0; m < arr.length; m++) {
+        switch (arr[m]) {
+            case '--discard-next':
+                m++;
+                break;
+            case '--discard-prev':
+                myArray.pop();
+                break;
+            case '--double-next':
+                if (m < arr.length - 1) myArray.push(arr[m + 1]);
+                break;
+            case '--double-prev':
+                if (m > 0) myArray.push(arr[m - 1]);
+                break;
+            default:
+                myArray.push(arr[m]);
+        };
+    }
+    return myArray;
 };
